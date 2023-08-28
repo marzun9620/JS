1afadf5dff5230c7614012c89773ddca1db8ce20
marzun9620 <202114048@student.mist.ac.bd>
2023-08-28 19:16:35 +0600

    it's Written on 28 aug 2023.

---
+++ eventModule.js
@@ -0,0 +1,6 @@
+// listener
+emitter.on('BellRang', ({ period }) => {
+    console.log(`Marzun ${period}`);
+});
+
+// raise an event
---
+++ eventModule_1.js
@@ -0,0 +1,12 @@
+const EventEmit = require('events');
+
+class Class extends EventEmit {
+    helloDot() {
+        setTimeout(() => {
+            this.emit('BellRang', {
+                period: 'fisrt',
+            });
+        }, 2000);
+    }
+}
+MediaElementAudioSourceNode;
