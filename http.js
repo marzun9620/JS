db522172ed0106ca06a4a0e2570f8dd81f73d67f
marzun9620 <202114048@student.mist.ac.bd>
2023-08-29 17:43:14 +0600

    Update http.js
    jj

--- http.js
+++ http.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-<<<<<<< HEAD
     if (req.url === '/') {
         res.write('<html><head><title>Form</title></head>');
         res.write('<body><form method="post" action="/about"><input name="message" /></form>');
@@ -16,20 +15,21 @@ const server = http.createServer((req, res) => {
             const parseBody = Buffer.concat(body).toString();
             console.log(parseBody);
         });
-=======
-    if (res.url === '/') {
-        res.write('Hello Word');
-        console.log('Marzun');
-        res.end();
-    } else if (res.url === '/about') {
-        res.write('I am marzun');
-        res.end();
->>>>>>> b3e0a65dcec64402b08feaf55e042df95fdbb899
-    } else {
-        res.write('Bye Friends');
-        res.end();
+
+        if (res.url === '/') {
+            res.write('Hello Word');
+            console.log('Marzun');
+            res.end();
+        } else if (res.url === '/about') {
+            res.write('I am marzun');
+            res.end();
+        } else {
+            res.write('Bye Friends');
+            res.end();
+        }
     }
 });
 
 server.listen(3000);
 console.log('Running Server');
+/// mmmm
